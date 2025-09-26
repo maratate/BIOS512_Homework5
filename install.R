73b6ffdd6e3df73f52fc156fcb7277e414651bcd
Mara Tate <89354755+maratate@users.noreply.github.com>
2025-09-25 20:51:38 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("Rtsne", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+
+IRkernel::installspec(user = FALSE)
